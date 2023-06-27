67502882af9d686737535f4de26f27f5f64b8853
marcus tourinho <viniciustourinho01@gmail.com>
2023-06-27 17:05:02 -0300

    Alerta de erros add

--- 2-front-end/secao-01/casa-de-cambio/src/main.js
+++ 2-front-end/secao-01/casa-de-cambio/src/main.js
@@ -11,7 +11,12 @@ const BASE_URL = 'https://api.exchangerate.host/latest?base=';
 const fetchAPI = (coin) => {
   return fetch(`${BASE_URL}${coin}`)
     .then((res) => res.json())
-    .then((data) => data.rates);
+    .then((data) => {
+      if (data.base !== coin.toUpperCase()) {
+        throw new Error('Moeda não existente');
+      }
+      return data.rates;
+    });
 };
 
 const renderCoins = (coins) => {
@@ -32,9 +37,31 @@ const renderCoins = (coins) => {
 
 const handleSearch = () => {
   const coin = currency.value;
+
+  if (!coin) {
+    Swal.fire({
+      icon: 'error',
+      title: 'Ops ...',
+      text: 'Você precisa passar uma moeda',
+      confirmButtonText: 'OK!',
+    });
+    return;
+  }
+
   title.innerHTML = `Valores referentes a 1 ${coin}`;
+
   fetchAPI(coin)
-    .then(renderCoins);
+    .then(renderCoins)
+    .catch((error) => {
+      title.innerHTML = '';
+      list.innerHTML = '';
+      Swal.fire({
+        icon: 'error',
+        title: 'Ops ...',
+        text: error.message,
+        confirmButtonText: 'OK!',
+      });
+    });
 };
 
 button.addEventListener('click', handleSearch);
