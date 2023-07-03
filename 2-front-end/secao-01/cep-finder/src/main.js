e3bca3f01e4c2079175b5581f01e5333ab2a90f8
marcus tourinho <viniciustourinho01@gmail.com>
2023-07-03 17:04:49 -0300

    fetch e response no console

--- 2-front-end/secao-01/cep-finder/src/main.js
+++ 2-front-end/secao-01/cep-finder/src/main.js
@@ -1,1 +1,18 @@
-import "style.css";
+import './style.css';
+
+const button = document.querySelector('button');
+const input = document.querySelector('input');
+
+const fetchAPI = async (cep) => {
+  const response = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
+  const data = await response.json();
+  return console.log(data);
+};
+
+const handleSearch = (event) => {
+  event.preventDefault();
+  const cep = input.value;
+  fetchAPI(cep);
+};
+
+button.addEventListener('click', handleSearch);
