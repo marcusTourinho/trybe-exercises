1b923f9c18078a9be6d6bf73f0c882f7e7e1727a
marcus tourinho <viniciustourinho01@gmail.com>
2023-07-03 17:26:15 -0300

    add new feature alerta de erros

--- 2-front-end/secao-01/cep-finder/src/main.js
+++ 2-front-end/secao-01/cep-finder/src/main.js
@@ -1,3 +1,4 @@
+import Swal from 'sweetalert2';
 import './style.css';
 
 const button = document.querySelector('button');
@@ -7,17 +8,18 @@ const pre = document.querySelector('pre');
 const fetchAPI = async (cep) => {
   const response = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
   const data = await response.json();
+  if (data.erro) throw new Error('Erro de cep inválido');
   pre.innerHTML = JSON.stringify(data);
+  return data;
 };
 
-const handleSearch = (event) => {
-  event.preventDefault();
+const handleSearch = async () => {
   const cep = input.value;
   try {
-    fetchAPI(cep);
+    await fetchAPI(cep);
   } catch (error) {
     pre.innerHTML = '';
-    return error.message;
+    Swal.fire('Ops', error.message, 'error');
   }
 };
 
