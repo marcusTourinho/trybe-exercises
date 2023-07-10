7bdeb8dbcd993cadffde1cbc779df3496e101b3b
marcus tourinho <viniciustourinho01@gmail.com>
2023-07-10 18:08:36 -0300

    CSS e funcionalidades

--- 2-front-end/secao-02/resume-register/resume-register/src/App.tsx
+++ 2-front-end/secao-02/resume-register/resume-register/src/App.tsx
@@ -1,35 +1,70 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [schooling, setSchooling] = useState('');
+  const [resume, setResume] = useState('');
+
+  function resetForm() {
+    setName('');
+    setEmail('');
+    setSchooling('Médio');
+    setResume('');
+  }
+
+  function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    resetForm();
+    // eslint-disable-next-line no-alert
+    window.alert(
+      `Nome: ${name}\nemail: ${email}
+      Escolaridade: ${schooling}\nExperiências: ${resume}`,
+    );
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <form onSubmit={ (event) => handleSubmit(event) }>
+      <label htmlFor="name">
+        Nome
+        <input
+          type="text"
+          id="name"
+          value={ name }
+          onChange={ ({ target }) => setName(target.value) }
+        />
+      </label>
+      <label htmlFor="email">
+        E-mail
+        <input
+          type="email"
+          id="email"
+          value={ email }
+          onChange={ ({ target }) => setEmail(target.value) }
+        />
+      </label>
+      <label>
+        Escoladirade
+        <select
+          value={ schooling }
+          onChange={ ({ target }) => setSchooling(target.value) }
+        >
+          <option value="Médio">Médio</option>
+          <option value="Superior">Superior</option>
+          <option value="Pós-graduação">Pós-graduação</option>
+        </select>
+      </label>
+      <label>
+        Resumo das experiências
+        <textarea
+          value={ resume }
+          onChange={ ({ target }) => setResume(target.value) }
+        />
+      </label>
+      <button>Enviar</button>
+    </form>
+  );
 }
 
-export default App
+export default App;
